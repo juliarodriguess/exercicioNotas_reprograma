477560386639d0954a199ffea874f71fe7986349
Julia Rodrigues <srodriguesjulia@gmail.com>
2018-10-03 21:56:07 -0300

    Calcular média de notas de alunos em tabela

---
+++ js/script.js
@@ -0,0 +1,16 @@
+const alunos = document.querySelectorAll(".aluno");
+for(let i=0; i < alunos.length; i++) {
+    let aluno = alunos[i];
+    const notaUm = aluno.querySelector(".info-notaum").textContent;
+    const notaDois = aluno.querySelector(".info-notadois").textContent;
+    let calculaMedia = (parseFloat(notaUm) + parseFloat(notaDois))/2;
+    media = aluno.querySelector(".info-media");
+    media.innerHTML = calculaMedia.toFixed(1);
+    if(media.innerHTML < 5) {
+        aluno.style.backgroundColor = "pink";
+    } else {
+        aluno.style.backgroundColor = "lightgreen";
+    }
+
+    
+}
